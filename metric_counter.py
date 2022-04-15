137b27a260c0cd179b8b41d1e73257aebf09f2ae
Do-Hyung <bagdohyeong0@gmail.com>
2022-04-15 18:11:47 +0900

    make key function

---
+++ metric_counter.py
@@ -0,0 +1,21 @@
+import ast
+
+
+def count_lines_of_code(filepath):
+    """ Count number of lines in code"""
+
+
+def count_blank(filepath):
+    """ Count number of blanks in file """
+
+
+def count_comment(filepath):
+    """ Count number of comments in file """
+
+
+def parse_file(filename):
+    """ Parsing file to AST """
+
+
+def count_function(filename):
+    """ Count number of functions in file with AST """
