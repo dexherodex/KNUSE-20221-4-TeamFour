02ea2776f2691a7e9777e7aeea9c0968c46208b5
Do-Hyung <bagdohyeong0@gmail.com>
2022-05-09 19:16:18 +0900

    feat(complexity_counter.py): add analyser function
    Adds function that count branch number, fan in number, fan out number of a function.

--- complexity_counter.py
+++ complexity_counter.py
@@ -17,3 +17,33 @@ def cyclomatic(branch):
 def ifc(fan_in, fan_out):
     ifc_complexity = (fan_in * fan_out) ** 2
     return ifc_complexity
+
+
+def analyser(function_item, branch, fan_in, fan_out):
+    for item in ast.walk(function_item):
+        item_copy = item
+        if isinstance(item_copy, (ast.While, ast.For, ast.If)):
+            branch += 1
+        elif isinstance(item_copy, ast.arguments):
+            fan_in += arguments_number(item_copy)
+        elif isinstance(item_copy, ast.Return):
+            fan_out += return_number(item_copy)
+
+    return branch, fan_in, fan_out
+
+
+def arguments_number(item_arguments):
+    return len(item_arguments.args)
+
+
+def return_number(item_return):
+    for item in ast.walk(item_return):
+        copy = item
+        if isinstance(copy, ast.Return):
+            continue
+        elif isinstance(copy, ast.Tuple):
+            return len(copy.elts)
+        else:
+            break
+    return 1
+
