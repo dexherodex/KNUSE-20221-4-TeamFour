936202222e876edef30bed55c906348e9141d6b3
Do-Hyung <bagdohyeong0@gmail.com>
2022-05-09 20:51:36 +0900

    feat(complexity_counter.py): modify function analyser
    Adds the ability to check the use of global variables.

--- complexity_counter.py
+++ complexity_counter.py
@@ -20,14 +20,22 @@ def ifc(fan_in, fan_out):
 
 
 def analyser(function_item, branch, fan_in, fan_out):
+    global_list = []
     for item in ast.walk(function_item):
         item_copy = item
         if isinstance(item_copy, (ast.While, ast.For, ast.If)):
             branch += 1
         elif isinstance(item_copy, ast.arguments):
             fan_in += arguments_number(item_copy)
+        elif isinstance(item_copy, ast.Global):
+            new_list = global_number(item_copy)
+            global_list.extend(new_list)
+            fan_in += len(new_list)
         elif isinstance(item_copy, ast.Return):
             fan_out += return_number(item_copy)
+        elif isinstance(item_copy, (ast.Assign, ast.AugAssign, ast.AnnAssign)):
+            if len(global_list) > 0:
+                fan_out += global_assign(item_copy, global_list)
 
     return branch, fan_in, fan_out
 
@@ -47,3 +55,24 @@ def return_number(item_return):
             break
     return 1
 
+
+def global_number(item_global):
+    return item_global.names
+
+
+def global_assign(item_assign, global_list):
+    count = 0
+    variables = item_assign.targets[0]
+    if isinstance(variables, ast.Name):
+        if variables.id in global_list:
+            count += 1
+    elif isinstance(variables, ast.Tuple):
+        for item in variables.elts:
+            if item.id in global_list:
+                count += 1
+
+    elif isinstance(variables, ast.Subscript):
+        if variables.value.id in global_list:
+            count += 1
+
+    return count
